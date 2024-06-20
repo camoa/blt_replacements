d0767ffe0e5f2e288d9f83d0ee7182e1f158995c
Carlos Ospina <camoa2005@gmail.com>
2024-06-20 13:38:55 -0500

    Update installthemes.sh for multisites

--- scripts/installthemes.sh
+++ scripts/installthemes.sh
@@ -3,11 +3,11 @@
 
 #locate the script in the themes folder
 cd docroot/themes/custom
-#themes=$(find .  -maxdepth 1 ! -path . -type d )
-#for theme in $themes; do
-    if [ ! -e "uswds_hhs/node_modules" ];then
-        cd uswds_hhs
+themes=$(find .  -maxdepth 1 ! -path . -type d )
+for theme in $themes; do
+    if [ ! -e "$theme/node_modules" ];then
+        cd $theme
         npm install
         cd ..
     fi
-#done
+done
