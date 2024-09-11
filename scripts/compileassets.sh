ba250c03fdd4c06ff615e6000b5e27e08e23afd7
Carlos Ospina <camoa2005@gmail.com>
2024-09-11 18:41:57 -0500

    Update compileassets.sh
    commenting

--- scripts/compileassets.sh
+++ scripts/compileassets.sh
@@ -8,6 +8,7 @@ themes=$(find . -maxdepth 1 ! -path . -type d)
 for theme in $themes; do
   if [ ! -e "$theme/css" ]; then
     cd "$theme"
+    #An example of a theme that uses a different command, could be extended with eleseif
     if [[ "$theme" == *"uswds"* ]]; then
       gulp compile
     else
